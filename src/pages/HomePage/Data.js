6ad1dbe873f9366ab453fce554498c1bae52c4dd
nexzanavod <aanavod@gmail.com>
2021-08-30 03:53:55 +0530

    first commit

---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,65 @@
+export const homeObjOne = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "With Chatty💬, ",
+  headline: "You'll get fast, simple, secure messaging for free ",
+  description: "",
+  buttonLabel: "Let's Chat",
+  imgStart: "",
+  img: require("../../images/Logo.gif"),
+  alt: "Credit Card",
+  start: "",
+};
+
+export const homeObjTwo = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Instant Setup",
+  headline: "Extremely quick onboarding process",
+  description:
+    "With Chatty💬, you'll get fast, simple, secure messaging for free*, available on phones & web browser all over the world.",
+  buttonLabel: "Learn More",
+  imgStart: "",
+  img: require("../../images/Group.jpg"),
+  alt: "Vault",
+  start: "",
+};
+
+export const homeObjThree = {
+  primary: false,
+  lightBg: true,
+  lightTopLine: false,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Alondra",
+  headline: "Headbands more than 500+ Designs.",
+  description: "Uniqe & wornderfull design for all of you.",
+  buttonLabel: "Click Here",
+  imgStart: "start",
+  img: require("../../images/Group.jpg"),
+  alt: "Vault",
+  start: "true",
+};
+
+export const homeObjFour = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Secure Database",
+  headline: "All your data is stored on our secure server",
+  description:
+    "You will never have to worry about your information getting leaked. Our team of security experts will ensure your records are kept safe.",
+  buttonLabel: "Sign Up Now",
+  imgStart: "start",
+  img: require("../../images/svg-3.svg"),
+  alt: "Vault",
+  start: "true",
+};
